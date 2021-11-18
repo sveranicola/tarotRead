d0f5e56222a835f46fd752be6dfc3ce321c91e8d
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-11-18 16:30:37 -0500

    this is a small change

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -5,8 +5,7 @@ import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 var App = () => {
   return (
     <div>
-      hello
-      <img src="https://cdn.pixabay.com/photo/2021/04/05/22/42/the-magician-6154763_1280.jpg" alt="the magician" />
+      Start page
     </div>
   )
 }
