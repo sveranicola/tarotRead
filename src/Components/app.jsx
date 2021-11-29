822843597b27856f77cdf9c8f3488388ceef4f56
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-11-29 18:31:03 -0500

    added home and new routes

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect} from 'react';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-
+import {BrowserRouter, Routes, Route, Switch, Link} from 'react-router-dom';
+import Home from './Home.jsx'
 
 var App = () => {
   return (
     <div>
       Tarot
       <BrowserRouter >
-        <Route path='/' exact>
-          Home
-        </Route>
+        <Routes>
+          <Route path='/' element={<Home />} exact />
+        </Routes>
       </BrowserRouter>
     </div>
   )
