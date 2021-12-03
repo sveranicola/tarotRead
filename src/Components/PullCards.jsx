fdea2839b0d2eff391406b44dcba322bd576b8db
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-12-03 17:05:52 -0500

    axios added

--- src/Components/PullCards.jsx
+++ src/Components/PullCards.jsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect} from 'react';
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import Axios from 'axios';
 
 
 var PullCards = () => {
+  const [pulled, addPulled] = useState([]);
+  const [flipped, addFlipped] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=78")
+    .then((res) => {
+      addPulled(res.data.cards);
+    })
+    .catch((err) => {throw(err)});
+  }, []);
+
   return (
     <div>
       Pull the cards
--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect} from 'react';
-import {BrowserRouter, Routes, Route, Switch, Link} from 'react-router-dom';
+import {HashRouter, Routes, Route, Switch, Link} from 'react-router-dom';
 import Home from './Components/Home.jsx'
 import PullCards from './Components/PullCards.jsx'
 
 var App = () => {
   return (
     <div>
-      <BrowserRouter >
+      <HashRouter >
         <Routes>
           <Route path='/' element={<Home />} exact />
         </Routes>
         <Routes>
           <Route path='/PullCards' element={<PullCards />} exact />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
